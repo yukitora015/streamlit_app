81b95ed7f9774a999c25da7db9ccd474881e1b54
yukitora015 <yukitora012345678@gmail.com>
2025-02-09 11:39:56 +0900

    second

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+#解答例
+
 import streamlit as st
+import random
+st.title("ランダム数字ジェネレーター")
+# 数字の個数を選択
+num_count = st.slider("表示する数字の個数", 1, 10, 5) #スライダーで数字の個数を選択
+
+if st.button("ランダムな数字を生成"):
+    for i in range(num_count):
+        random_number = random.randint(1, 100)
+    st.write(f"生成された数字: {random_number}")
